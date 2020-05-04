354b8c1ce41092e77994709dd3612d65b8eabf24
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-05-04 12:27:33 +0200

    2nd example

---
+++ unittest_mock/mock-test2/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""Vstupní bod do testované aplikace."""
+
+from application import *
+
+
+if __name__ == '__main__':
+    # pouze zavoláme funkci, která se bude v testech mockovat
+    print(function1())
