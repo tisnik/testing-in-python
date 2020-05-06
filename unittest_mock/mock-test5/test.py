116b34a7d356d247a2df404de126f9c705682463
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-05-06 12:31:18 +0200

    Mocking example #5

---
+++ unittest_mock/mock-test5/test.py
@@ -0,0 +1,44 @@
+"""Implementace jednotkových testů."""
+
+from unittest.mock import *
+
+import application
+
+
+def test1():
+    """První test neprovádí prakticky žádné reálné kontroly, jen zavolá testovanou funkci."""
+    print("function1 returns: {v}".format(v=application.function1()))
+
+
+@patch('application.function2', return_value=42)
+def test2(mocked_function):
+    # vytiskneme informaci o tom, zda se mockovaná funkce zavolala
+    print("mocked function called: {c}".format(c=mocked_function.called))
+    print("function1 returns: {v}".format(v=application.function1()))
+    # opět vytiskneme informaci o tom, zda se mockovaná funkce zavolala
+    print("mocked function called: {c}".format(c=mocked_function.called))
+
+
+def side_effect_handler():
+    print("side_effect_handler function called")
+    return -1
+
+
+@patch('application.function2', side_effect=side_effect_handler)
+def test3(mocked_function):
+    # vytiskneme informaci o tom, zda se mockovaná funkce zavolala
+    print("mocked function called: {c}".format(c=mocked_function.called))
+    print("function1 returns: {v}".format(v=application.function1()))
+    # opět vytiskneme informaci o tom, zda se mockovaná funkce zavolala
+    print("mocked function called: {c}".format(c=mocked_function.called))
+
+
+if __name__ == '__main__':
+    test1()
+    print()
+
+    test2()
+    print()
+
+    test3()
+    print()
