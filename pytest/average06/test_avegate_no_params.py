743b3bbd4b03c8dac5158c917e27e57876b6fa63
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-05-18 10:31:37 +0200

    Unit tests for function to compute average value, example #6

---
+++ pytest/average06/test_avegate_no_params.py
@@ -0,0 +1,19 @@
+"""Implementace jednotkových testů."""
+
+import pytest
+
+from average import average
+
+
+def test_average_basic_more_checks():
+    """Otestování výpočtu průměru."""
+    inputs = (
+            (0, 0),
+            (1, 0),
+            (1, 1),
+            (1, 2))
+    expected_results = (0, 0.5, 1, 1.5)
+
+    for input, expected in zip(inputs, expected_results):
+        result = average(input)
+        assert result == expected, "Očekávaná hodnota {}, vráceno {}".format(expected, result)
