69768b83793974a87e520c85fcad4a5284f0d79b
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-05-20 07:40:55 +0200

    Fixed pylinter issues

--- pytest/primes6/primes.py
+++ pytest/primes6/primes.py
@@ -4,13 +4,13 @@
 # originální kód lze nalézt na adrese:
 # http://www.rosettacode.org/wiki/Sieve_of_Eratosthenes#Using_array_lookup
 def primes2_(limit):
-    is_prime = [False] * 2 + [True] * (limit - 1) 
-    for n in range(int(limit**0.5 + 1.5)): # stop at ``sqrt(limit)``
+    """Výpočet seznamu prvočísel až do zadaného limitu."""
+    is_prime = [False] * 2 + [True] * (limit - 1)
+    for n in range(int(limit**0.5 + 1.5)):  # stop at ``sqrt(limit)``
         if is_prime[n]:
             for i in range(n*n, limit+1, n):
                 is_prime[i] = False
     return [i for i, prime in enumerate(is_prime) if prime]
-
 
 
 # originální kód lze nalézt na adrese:
