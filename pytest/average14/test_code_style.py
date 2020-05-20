7f0f965cd1a65707d65ecf2a717f8a5a3de8b531
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-05-20 10:37:18 +0200

    Updated average test #14

---
+++ pytest/average14/test_code_style.py
@@ -0,0 +1,17 @@
+"""Implementace jednotkových testů."""
+
+from pathlib import Path
+from sys import exit
+import pycodestyle
+import pytest
+
+from average import average
+
+
+def test_code_style():
+    files = list(Path(".").rglob("*.py"))
+
+    style = pycodestyle.StyleGuide(quiet=False, config_file='setup.cfg')
+    result = style.check_files(files)
+    print("Total errors:", result.total_errors)
+    assert result.total_errors == 0, "Detected {} code style problems".format(result.total_errors)
