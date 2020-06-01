ee27cc4a01a7b13e2649d7c5b9274bf4eb3d7334
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-01 10:20:59 +0200

    Adder with type checks

---
+++ mypy/adder.py
@@ -0,0 +1,20 @@
+from typing import TypeVar
+
+IntOrString = TypeVar("IntOrString", str, int)
+
+
+def add(x: IntOrString, y: IntOrString) -> IntOrString:
+    return x + y
+
+
+print(add(1, 2))
+print(add("foo", "bar"))
+
+print(add("foo", 2))
+print(add(1, "bar"))
+
+print(42 + add(1,2))
+print(42 + add("foo", "bar"))
+
+print("result: " + add("foo", "bar"))
+print("result: " + add(1, 2))
