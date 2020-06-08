607f340c405c640370563a6c7bd6eb2060475b60
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-08 10:53:11 +0200

    Broken ROT13 algorithm tested by configurable Hypothesis tests

---
+++ hypothesis/rot13_3/rot13_test.py
@@ -0,0 +1,13 @@
+"""Test jednoduché šifry typu ROT13."""
+
+from hypothesis import given, note, settings
+from hypothesis.strategies import text, characters
+
+from rot13 import rot13
+
+
+@given(text(characters(min_codepoint=32, max_codepoint=127), min_size=5))
+@settings(max_examples=500)
+def test_rot13(s):
+    """Test jednoduché šifry typu ROT13."""
+    assert rot13(rot13(s)) == s
