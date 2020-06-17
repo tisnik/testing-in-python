8a8a2ddce42269d1335e33a5028083bb04dbe46d
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-17 10:36:42 +0200

    Fixes issue #55

--- hypothesis/bubble_sort_6/bubble_sort_test.py
+++ hypothesis/bubble_sort_6/bubble_sort_test.py
@@ -9,4 +9,4 @@ from bubble_sort import bubble_sort
 @given(lists(integers(), min_size=5))
 def test_bubble_sort(alist):
     sorted = bubble_sort(alist)
-    assert all(x<=y for x, y in zip(sorted, sorted[1:]))
+    assert all(x <= y for x, y in zip(sorted, sorted[1:]))
