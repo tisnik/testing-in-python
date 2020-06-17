b6b71570984fa87b3a2f8c1c0ded6a2edaa9142e
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-17 10:38:03 +0200

    Fixes issue #50

--- hypothesis/map2.py
+++ hypothesis/map2.py
@@ -1,6 +1,7 @@
 from hypothesis.strategies import lists, integers
 
-g = lists(integers().filter(lambda x: x > 0 and x % 2 == 0)).filter(lambda x: len(x) > 2 and len(x) < 6).map(sorted)
+g = lists(integers().filter(lambda x: x > 0 and x % 2 == 0)).filter(
+        lambda x: len(x) > 2 and len(x) < 6).map(sorted)
 
 for _ in range(10):
     print(g.example())
