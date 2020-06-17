95c45c4940e753404237181350e2e7811d2de0c4
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-17 10:23:20 +0200

    Fixes issue #45

--- hypothesis/numpy2.py
+++ hypothesis/numpy2.py
@@ -1,7 +1,7 @@
 import numpy as np
 from hypothesis.extra.numpy import arrays
 
-g = arrays(np.float, (4,3), elements=None)
+g = arrays(np.float, (4, 3), elements=None)
 
 for _ in range(10):
     print(g.example())
