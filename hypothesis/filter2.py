fecad0fbc46161d45ca776afedb3fa75fbe5e6db
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-17 10:37:07 +0200

    Fixes issue #53

--- hypothesis/filter2.py
+++ hypothesis/filter2.py
@@ -1,6 +1,7 @@
 from hypothesis.strategies import lists, integers
 
-g = lists(integers().filter(lambda x: x > 0 and x % 2 == 0)).filter(lambda x: len(x) > 2 and len(x) < 6)
+g = lists(integers().filter(
+    lambda x: x > 0 and x % 2 == 0)).filter(lambda x: len(x) > 2 and len(x) < 6)
 
 for _ in range(10):
     print(g.example())
