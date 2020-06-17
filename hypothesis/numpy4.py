ebd91ae3fc6021640addbfa894b9d8fdb31959e6
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-17 10:28:29 +0200

    Fixes issues #42 and #43

--- hypothesis/numpy4.py
+++ hypothesis/numpy4.py
@@ -1,7 +1,7 @@
 import numpy as np
 from hypothesis.extra.numpy import arrays
 
-g = arrays(np.half, (2,3,4), elements=None, unique=True)
+g = arrays(np.half, (2, 3, 4), elements=None, unique=True)
 
 for _ in range(10):
     print(g.example())
