70bff6131d96a82b924fe19366db6f47c1fa6ea4
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-17 10:21:32 +0200

    Fixes issues #46 and #47

--- hypothesis/numpy5.py
+++ hypothesis/numpy5.py
@@ -2,7 +2,7 @@ import numpy as np
 from hypothesis.strategies import integers
 from hypothesis.extra.numpy import arrays
 
-g = arrays(np.int32, (2,3,4), elements=integers(0, 100), unique=True)
+g = arrays(np.int32, (2, 3, 4), elements=integers(0, 100), unique=True)
 
 for _ in range(10):
     print(g.example())
