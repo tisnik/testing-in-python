9533c4b83b84cb3af2ae40ebb7e67e2d8dc8c2ae
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-17 10:37:46 +0200

    Fixes issue #51

--- hypothesis/from_regex_4.py
+++ hypothesis/from_regex_4.py
@@ -1,7 +1,7 @@
 from hypothesis.strategies import from_regex
 
 g = from_regex(r"[^@\s]+@[^@\s]+\.[a-zA-Z0-9]+$", fullmatch=True).filter(
-        lambda s:all(ord(c) < 128 for c in s))
+        lambda s: all(ord(c) < 128 for c in s))
 
 for _ in range(20):
     print(g.example())
