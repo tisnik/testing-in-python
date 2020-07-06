95d0ecb247de850fddc5b135836d8ae5931ae743
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-07-06 09:22:45 +0200

    7th example: HTTP POST method with payload send via form parameters

---
+++ requests/07_post_method.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import requests
+
+# adresa s testovaci REST API sluzbou
+URL = "http://httpbin.org/post"
+
+# poslani HTTP dotazu typu POST
+response = requests.post(URL)
+
+# vypis odpovedi v plain textu
+print(response.text)
