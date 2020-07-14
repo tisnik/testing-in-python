28f43b5861738bc9c9c8c97ec69d9175c0be636b
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-07-14 08:55:42 +0200

    1st example: basic usage of requests for GET method

---
+++ requests/tests/01_basic_usage_test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import requests
+
+
+def test_get_method_for_existing_endpoint():
+    # adresa s testovaci REST API sluzbou
+    URL = "http://httpbin.org/get"
+
+    # poslani HTTP dotazu typu GET
+    response = requests.get(URL)
+
+    assert response is not None
+
+
+def test_get_method_for_missing_endpoint():
+    # adresa s testovaci REST API sluzbou
+    URL = "http://httpbin.org/neexistuje"
+
+    # poslani HTTP dotazu typu GET
+    response = requests.get(URL)
+
+    assert response is not None
