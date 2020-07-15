636311a3e9181896a3b4795e2499d495ee571a6d
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-07-15 16:54:29 +0200

    Simpler variant of example #8

---
+++ requests/tests/08_check_json_content_B.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import requests
+
+
+def expect_ok_response(response):
+    assert response is not None
+    assert response.ok
+    assert response.status_code == 200
+
+
+def expect_content_type(response, content_type):
+    # precteni hlavicek
+    headers = response.headers
+
+    # test existence hlavicky
+    assert "content-type" in headers
+
+    # kontrola obsahu hlavicky
+    assert headers["content-type"] == content_type
+
+
+def test_get_method_for_existing_endpoint():
+    # adresa s testovaci REST API sluzbou
+    URL = "http://httpbin.org/get?x=6&y=7&answer=42"
+
+    # poslani HTTP dotazu typu GET
+    response = requests.get(URL)
+
+    # zakladni test odpovedi
+    expect_ok_response(response)
+    expect_content_type(response, "application/json")
+
+    # pruchod datovou strukturou odeslanou serverem
+    encoded = response.json()
+
+    assert encoded["args"]["answer"] == "42"
